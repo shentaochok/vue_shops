d7b83faeaaf4ed41de7dc24c15b96dfcef35f87a
沈涛 <13505146451@163.com>
2019-05-27 19:57:32 +0800

    first day

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <div style="font-size:30px;">{{count}}</div>
+    <p>
+      <button @click="$store.commit('add')">add</button>
+      <button @click="$store.commit('reduce')">reduce</button>
+    </p>
+    <header-nav></header-nav>
+    <shop></shop>
+  </div>
+</template>
+
+<script>
+import headerNav from '@/components/header-nav';
+import shop from '@/view/shop';
+import {mapState} from 'vuex';
+export default {
+  name: 'App',
+  computed:mapState({
+     count:state=>state.count
+  }),
+  components:{
+     headerNav,
+     shop
+  },
+  methods:{
+    
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/view/shop-item.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="item">
+    <div>
+      <div class="item-img">
+        <img
+          alt="Smartisan 快充移动电源 10000mAh"
+          :src="item.sku_info[imageIndex].ali_image+'?x-oss-process=image/resize,w_206/quality,Q_80/format,webp'"
+          style="opacity: 1;"
+        >
+      </div>
+      <h6>{{item.name}}</h6>item
+      <h3>{{item.name_title}}</h3>
+      <div class="params-colors">
+        <ul class="colors-list">
+          <li v-for="color,index in item.sku_info">
+            <a href="javascript:;" :class="{active:index==imageIndex}" @click="tabIndex(index)">
+              <img :src="'http://img01.smartisanos.cn/'+color.spec_json.image+'20X20.jpg'">
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div class="item-btns clearfix">
+        <span class="item-gray-btn">
+          <a href="javascript:;" target="_blank">查看详情</a>
+        </span>
+        <span class="item-blue-btn">加入购物车</span>
+      </div>
+      <div class="item-price clearfix">
+        <i>¥</i>
+        <span>{{item.price}}</span>
+      </div>
+      <div class="discount-icon">false</div>
+      <div class="item-cover">
+        <a href="javascript:;" target="_blank"></a>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data(){
+    return{
+        imageIndex:0
+    }
+  },
+  props: ["item"],
+  methods: {
+    tabIndex(index) {
+      this.imageIndex = index;
+    }
+  }
+};
+</script>
+<style>
+</style>
+
+
